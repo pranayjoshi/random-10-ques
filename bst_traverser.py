707f7bdbba74cc73b43cdb2e04a56256221b0ef7
Pranay Joshi <35572191+pranayteaches@users.noreply.github.com>
2019-08-12 21:26:49 +0530

    Create bst_traverser.py

---
+++ bst_traverser.py
@@ -0,0 +1,26 @@
+
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def printInorder(root):
+    if root:
+        printInorder(root.right)
+        print(root.val)
+        printInorder(root.left)
+
+
+
+# Driver code
+root = Node(1)
+root.left      = Node(2)
+root.right     = Node(3)
+root.left.left  = Node(4)
+root.left.right  = Node(5)
+root.right.left  = Node(6)
+root.right.right  = Node(7)
+
+print("\nImplemented Backward Iterator in BST")
+printInorder(root)
